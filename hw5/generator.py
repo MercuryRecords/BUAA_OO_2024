e81f1044a58edac99899b02fb149278038abb588
李泰川 <21371285@buaa.edu.cn>
2024-03-31 18:11:03 +0800

    hw5 - 互测

--- hw5/generator.py
+++ hw5/generator.py
@@ -1,37 +1,50 @@
 from random import randint, random
 
-MAX_TIME = 20
+MAX_TIME = 50
 MAX_FLOOR = 11
 MIN_FLOOR = 1
 MAX_ELEVATOR = 6
 MIN_ELEVATOR = 1
 
-def chooseFloor(min,max):
-    start, end = randint(min, max), randint(min, max)
+
+def chooseFloor(_min, _max):
+    start, end = randint(_min, _max), randint(_min, _max)
     while start == end:
-            start, end = randint(min, max), randint(min, max)
-    return start,end
+        start, end = randint(_min, _max), randint(_min, _max)
+    return start, end
+
 
 def chooseTime():
-    return MAX_TIME * random() + 1 
+    return MAX_TIME * random() + 1
+
 
 def chooseBy():
     return randint(MIN_ELEVATOR, MAX_ELEVATOR)
 
-def genData(length):
+
+def genData(length=70):
+    length = min(length, 30 * MAX_ELEVATOR - MIN_ELEVATOR + 1)
     ans = []
-    # ans is a list of time,id,start,end,by
+    requests_by_elevator = [0 for _ in range(MAX_ELEVATOR + 1)]
+    # ans is a list of time, id, start, end, by
 
     for i in range(length):
         time = chooseTime()
-        start,end = chooseFloor(MIN_ELEVATOR,MAX_FLOOR)
+        start, end = chooseFloor(MIN_ELEVATOR, MAX_FLOOR)
         by = chooseBy()
         ans.append((time,f"[{time:.1f}]{i + 1}-FROM-{start}-TO-{end}-BY-{by}\n"))
+        while requests_by_elevator[by] >= 30:
+            by = chooseBy()
+        requests_by_elevator[by] += 1
+        requests_by_elevator[0] += 1
+        ans.append((time, f"[{time:.1f}]{i}-FROM-{start}-TO-{end}-BY-{by}\n"))
     ans.sort(key=lambda x: x[0])
+
+    # for i in range(length):
+    #     print(ans[i][1], end="")
+
     return [item[1] for item in ans]
 
 
 if __name__ == "__main__":
-    ans = genData(20)
-    for i in range(20):
-        print(ans[i],end="")
+    ans = genData(70)
---
+++ hw5/mutualTest.py
@@ -0,0 +1,128 @@
+import datetime
+import os
+import platform
+import subprocess
+import multiprocessing
+import shutil
+import time
+from random import randint
+
+from generator import genData
+
+if platform.system() == 'Windows':
+    FEED_PROGRAM = './datainput_student_win64.exe'
+else:
+    FEED_PROGRAM = './datainput_student_linux_x86_64'
+CACHE_PATH = "cache"
+if platform.system() == 'Windows':
+    C_NAME = 'checker.exe'
+else:
+    C_NAME = 'checker'
+
+
+# 假设这是您要对每个.jar文件执行的函数
+def process_jar_file(jar_file_path, cache_folder, stdin_path):
+    # 在这里添加处理.jar文件的代码
+    # print(f"Processing {jar_file_path}")
+    # run your program
+    stdout_path = os.path.join(cache_folder, f"stdout_{jar_file_path}.txt")
+    with open(stdout_path, "w") as stdout_file:
+        datainput_proc = subprocess.Popen([FEED_PROGRAM], cwd=cache_folder, stdout=subprocess.PIPE,
+                                          stderr=subprocess.STDOUT)
+        java_proc = subprocess.Popen(["java", "-jar", jar_file_path], stdin=datainput_proc.stdout,
+                                     stdout=stdout_file, stderr=subprocess.STDOUT, shell=True)
+
+    try:
+        return_code = java_proc.wait(timeout=120)
+        if return_code != 0:
+            return f"Error-{return_code}"
+    except subprocess.TimeoutExpired:
+        return "OverTime1"
+
+    # 检查子进程是否已经结束
+    if java_proc.poll() is None:
+        return "OverTime2"
+    if java_proc.poll() != 0:
+        return "OverTime3"
+    if java_proc.stderr:
+        return "Error2"
+
+    # 运行 checker，传递 stdin.txt 和 stdout.txt 的路径作为命令行参数
+    checker_output = subprocess.run([f"./{C_NAME}", stdin_path, stdout_path], capture_output=True,
+                                    text=True).stdout.strip()
+    if checker_output != "Correct.":
+        print(jar_file_path, checker_output)
+        return "Error3"
+    else:
+        return "Correct"
+
+
+# 获取当前目录下所有的.jar文件
+def get_jar_files(directory):
+    jar_files = [f for f in os.listdir(directory) if f.endswith('.jar')]
+    return jar_files
+
+
+# 创建并启动一个进程来处理每个.jar文件
+def start_processes(jar_files):
+    cnt = 1
+    processor_id = randint(0, 100000)
+
+    while True:
+        cache_folder = os.path.join(CACHE_PATH, f"{processor_id}_iteration_{cnt}")
+        try:
+            os.makedirs(cache_folder, exist_ok=True)
+            cnt += 1
+
+            # genData
+            stdin_path = os.path.join(cache_folder, f"stdin.txt")
+            with open(stdin_path, "w") as f:
+                tmp_stdin = genData()
+                for entry in tmp_stdin:
+                    f.write(entry)
+
+            # stdin_path = os.path.join(os.getcwd(), "stdin.txt")
+            shutil.copy(FEED_PROGRAM, cache_folder)
+            # shutil.copy(stdin_path, cache_folder)
+
+            with multiprocessing.Pool() as pool:
+                # 使用列表推导式创建一个包含所有任务的列表
+                tasks = [(jar_file, pool.apply_async(process_jar_file, (jar_file, cache_folder, stdin_path))) for
+                         jar_file in jar_files]
+
+                # 遍历所有任务，并获取结果或处理超时
+                time_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+                for fname, task in tasks:
+                    print(fname)
+                    try:
+                        # 尝试获取结果，设置超时时间
+                        result = task.get()
+                        if result != "Correct":
+                            with open(f"{result}_by_{fname}_at_{time_str}.txt", "w") as fout, open(stdin_path, 'r') as fin:
+                                fout.write(fin.read())
+                    except KeyboardInterrupt:
+                        shutil.rmtree(cache_folder)
+                        pass
+
+            shutil.rmtree(cache_folder)
+            pass
+        except KeyboardInterrupt:
+            shutil.rmtree(cache_folder)
+            pass
+
+
+# 主函数
+def main():
+    directory = os.getcwd()  # 获取当前工作目录
+    jar_files = get_jar_files(directory)
+
+    if not jar_files:
+        print("No .jar files found in the current directory.")
+        return
+
+    print(jar_files)
+    start_processes(jar_files)
+
+
+if __name__ == "__main__":
+    main()
